d219056ad81720f552c8f06e63acd7f4fa8d7236
Bruno Martins <bgcngm@gmail.com>
2024-09-29 10:37:34 +0100

    gts4lv: Regen for ELF checks
    Change-Id: Ia28d16efb4d08a54985a72f345735205c63e4e8f

--- gts4lv-vendor.mk
+++ gts4lv-vendor.mk
@@ -6,10 +6,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/samsung/gts4lv
 
 PRODUCT_COPY_FILES += \
-    vendor/samsung/gts4lv/proprietary/vendor/bin/ATFWD-daemon:$(TARGET_COPY_OUT_VENDOR)/bin/ATFWD-daemon \
-    vendor/samsung/gts4lv/proprietary/vendor/bin/adpl:$(TARGET_COPY_OUT_VENDOR)/bin/adpl \
-    vendor/samsung/gts4lv/proprietary/vendor/bin/hw/rild:$(TARGET_COPY_OUT_VENDOR)/bin/hw/rild \
-    vendor/samsung/gts4lv/proprietary/vendor/bin/netmgrd:$(TARGET_COPY_OUT_VENDOR)/bin/netmgrd \
     vendor/samsung/gts4lv/proprietary/vendor/etc/audconf/ODM/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Bluetooth_cal.acdb \
     vendor/samsung/gts4lv/proprietary/vendor/etc/audconf/ODM/General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/General_cal.acdb \
     vendor/samsung/gts4lv/proprietary/vendor/etc/audconf/ODM/Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Global_cal.acdb \
@@ -60,27 +56,29 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gts4lv/proprietary/vendor/firmware/wlanmdsp_SM-T727.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/wlanmdsp_SM-T727.mbn \
     vendor/samsung/gts4lv/proprietary/vendor/firmware/wlanmdsp_SM-T727R4.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/wlanmdsp_SM-T727R4.mbn \
     vendor/samsung/gts4lv/proprietary/vendor/firmware/wlanmdsp_SM-T727U.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/wlanmdsp_SM-T727U.mbn \
-    vendor/samsung/gts4lv/proprietary/vendor/firmware/wlanmdsp_SM-T727V.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/wlanmdsp_SM-T727V.mbn \
-    vendor/samsung/gts4lv/proprietary/vendor/lib/vendor.samsung.hardware.radio.bridge@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.samsung.hardware.radio.bridge@2.0.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib/vendor.samsung.hardware.radio.bridge@2.1.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.samsung.hardware.radio.bridge@2.1.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libconfigdb.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libconfigdb.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libdsi_netctrl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdsi_netctrl.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libengmode_client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libengmode_client.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/liblqe.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblqe.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libnetmgr.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libnetmgr.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libnetmgr_common.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libnetmgr_common.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libnlnetmgr.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libnlnetmgr.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libpdnotifier.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libpdnotifier.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libqmiextservices.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqmiextservices.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libqmiservices-sem-ext.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqmiservices-sem-ext.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libsec-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsec-ril.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libsystem_health_mon.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsystem_health_mon.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libvkmanager_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvkmanager_vendor.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/libxml.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libxml.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/vendor.qti.hardware.radio.atcmdfwd@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.radio.atcmdfwd@1.0.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/vendor.samsung.hardware.radio.bridge@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.samsung.hardware.radio.bridge@2.0.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/vendor.samsung.hardware.radio.bridge@2.1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.samsung.hardware.radio.bridge@2.1.so \
-    vendor/samsung/gts4lv/proprietary/vendor/lib64/vendor.samsung.hardware.radio.channel@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.samsung.hardware.radio.channel@2.0.so
+    vendor/samsung/gts4lv/proprietary/vendor/firmware/wlanmdsp_SM-T727V.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/wlanmdsp_SM-T727V.mbn
 
 PRODUCT_PACKAGES += \
-    libril
+    libril \
+    vendor.samsung.hardware.radio.bridge@2.0 \
+    vendor.samsung.hardware.radio.bridge@2.1 \
+    libconfigdb \
+    libdsi_netctrl \
+    libengmode_client \
+    liblqe \
+    libnetmgr \
+    libnetmgr_common \
+    libnlnetmgr \
+    libpdnotifier \
+    libqmiextservices \
+    libqmiservices-sem-ext \
+    libsec-ril \
+    libsystem_health_mon \
+    libvkmanager_vendor \
+    libxml \
+    vendor.qti.hardware.radio.atcmdfwd@1.0 \
+    vendor.samsung.hardware.radio.channel@2.0 \
+    ATFWD-daemon \
+    adpl \
+    rild \
+    netmgrd
